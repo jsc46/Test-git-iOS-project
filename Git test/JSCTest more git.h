79a01792efbbf4b63e7e3f87323de6f788cbb2c1
jsc46 <jsegarrac@gmail.com>
2014-03-31 16:06:31 +0200

    added a test class

---
+++ Git test/JSCTest more git.h
@@ -0,0 +1,13 @@
+//
+//  JSCTest more git.h
+//  Git test
+//
+//  Created by Joan Segarra on 31/03/14.
+//  Copyright (c) 2014 pucesllamineres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSCTest_more_git : NSObject
+
+@end
---
+++ Git test/JSCTest more git.m
@@ -0,0 +1,13 @@
+//
+//  JSCTest more git.m
+//  Git test
+//
+//  Created by Joan Segarra on 31/03/14.
+//  Copyright (c) 2014 pucesllamineres. All rights reserved.
+//
+
+#import "JSCTest more git.h"
+
+@implementation JSCTest_more_git
+
+@end
